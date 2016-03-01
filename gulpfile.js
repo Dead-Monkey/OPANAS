9fded19bc4047e54de4a84844f4775ed84567306
edish edish <edish edish>
2016-03-01 16:45:47 +0200

    checkpoint

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,18 @@
-'use strict';
-
-var gulp = require('gulp'),
+let gulp = require('gulp'),
   rename = require('gulp-rename'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
-  gutil = require('gulp-util'),
-  ts = require('gulp-typescript'),
-  sourcemaps = require('gulp-sourcemaps');
+  gutil = require('gulp-util');
 
-gulp.task('scripts', function() {
-  return gulp.src([
-      "node_modules/es6-shim/es6-shim.min.js",
-      "node_modules/systemjs/dist/system-polyfills.js",
-      "node_modules/angular2/bundles/angular2-polyfills.js",
-      "node_modules/rxjs/bundles/Rx.js",
-      "node_modules/angular2/bundles/angular2.dev.js"
+  gulp.task('lib',function() {
+    gulp.src([
+      'node_modules/es6-shim/es6-shim.min.js',
+      'node_modules/systemjs/dist/system-polyfills.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js',
+      'node_modules/angular2/bundles/router.dev.js'
     ])
-    .pipe(concat('main.js'))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(uglify().on('error', gutil.log)) //try without uglify ))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('scripts:ts', function() {
-  var tsProject = ts.createProject('tsconfig.json', {
-    outFile: 'bla.js',
-    typescript: require('typescript')
-  });
-
-  var tsResult = gulp.src('app/**/*.ts')
-    .pipe(sourcemaps.init())
-    .pipe(ts(tsProject));
-
-  return tsResult.js
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist/build/lib'))
+  })
